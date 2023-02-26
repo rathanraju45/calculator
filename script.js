00ea5acddf17e5c18d5d47d7e0f0fe9b6f30f087
rathanraju45 <111771258+rathanraju45@users.noreply.github.com>
2023-02-26 21:46:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function matrixDisplay() {
+  var isClicked=true
+  var matrixTitle = document.querySelector(".matrix");
+  if (isClicked){
+    matrixTitle.style.display="block"
+  }
+  else{
+    matrixTitle.style.display="none"
+  }
+}
